495cc0414a7198b8278e555f2106f028018874ee
anannanur <anannaanu974@gmail.com>
2023-01-23 13:03:47 +0600

    product-card and top-rated page done

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,16 +1,23 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+
+import React, { createContext, useContext, useEffect, useReducer} from 'react';
+import { actionTypes } from '../state/actionTypes';
+import { initialState, productReducer } from '../state/productReducer';
 const PRODUCT_CONTEXT = createContext()
 
 const ProductProvider = ({children}) => {
-    const [data, setData] = useState([])
+    
+    const [state, dispatch] = useReducer(productReducer, initialState)
     useEffect(() => {
+      dispatch({type : actionTypes.FETCHING_START})
       fetch('products.json')
         .then(res => res.json())
-        .then(data => setData(data))
+        .then(data => dispatch({type : actionTypes.FETCHING_SUCCESS, payload : data}))
+        .catch(()=> dispatch({type : actionTypes.FETCHING_ERROR}))
     }, [])
-  
+    console.log(state)
     const value ={
-      data
+      state,
+      dispatch
     }
     return (
         <PRODUCT_CONTEXT.Provider value={value}>{children}</PRODUCT_CONTEXT.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
 import { useProducts } from '../context/ProductProvider';
 
 const Home = () => {
-    const {data} = useProducts();
-    console.log(data)
+    const { state: { products, loading, error } } = useProducts();
+    let content;
+    if (loading) {
+        content = <p>Loading</p>
+    }
+    if (error) {
+        content = <p>Something went wrong</p>
+    }
+    if (!loading && !error && products.length === 0) {
+        content = <p>No product remains here. List is empty</p>
+    }
+    if (!loading && !error && products.length) {
+        content = products.map(product => <ProductCard product={product} key={product.price}></ProductCard>)
+    }
     return (
-        <div>
-            <h1>This is Home Page</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14 max-w-7xl mx-auto my-10'>
+            {
+                content
+            }
         </div>
     );
 };
